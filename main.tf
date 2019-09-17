9203570ef1c7cb6fe701679d9e3e21d6f222fc55
Takayuki Kaburagi <t.kaburagi@me.com>
2019-09-17 22:08:07 +0900

    null-test

--- main.tf
+++ main.tf
@@ -1,9 +1,50 @@
 terraform {
-  required_version = " 0.12.6"
+  required_version = "~> 0.12"
 }
 
 provider "aws" {
 	access_key = var.access_key
 	secret_key = var.secret_key
 	region = var.region
+}
+
+
+resource "null_resource" "invoked_by_user_creation" {
+	triggers {
+		endpoint = aws_iam_user.lb.id
+	}
+	command = "echo INVOKED"
+}
+
+resource "aws_iam_user" "lb" {
+	name = "loadbalancer"
+	path = "/system/"
+
+	tags = {
+		tag-key = "tag-value"
+	}
+}
+
+resource "aws_iam_access_key" "lb" {
+	user = aws_iam_user.lb.name
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+	name = "test"
+	user = aws_iam_user.lb.name
+
+	policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
